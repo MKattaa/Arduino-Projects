a32325219574fef61f34ed32970ead158482a59c
Mohamad Kattaa <Mohamad@fabrik19.local>
2020-07-22 15:53:10 +0200

    ibis server

---
+++ IBIS_Server/IBIS_Server.ino
@@ -0,0 +1,80 @@
+#include <UIPEthernet.h> // Used for Ethernet
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+// **** ETHERNET SETTING ****
+byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };                                       
+IPAddress ip(192, 168, 1, 200);                        
+EthernetServer server(8080);
+
+void setup() {
+  Serial.begin(9600);
+  
+  // start sensors
+  sensors.begin();
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+
+  Serial.print("Server IP Address: ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+
+  if (client) 
+  {  
+    Serial.println("New Connection with andriod TV screen");
+
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) 
+        {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");  // the connection will be closed after completion of the response         
+          client.println();
+          sensors.requestTemperatures();
+          client.println("{\"temp\": " + String(sensors.getTempCByIndex(0)) + ", \"HalteStatus\": " + "Wagenhält" + ", \"nächsteHalteStelle\": " + "Berliner Platz" + ", \"Karte_Id\": " + "892354" + "}");
+          client.println();
+          break;
+        }
+
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') 
+        {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      } 
+    }
+
+    // give the web browser time to receive the data
+    delay(10);
+
+    // close the connection:
+    client.stop();
+    Serial.println("Disconnected\n");
+  }
+}
